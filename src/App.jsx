b9a8b4b8247c714b262eac93e0ba38b407d02f48
Mradul2006-Dev <mradulsharma272006@gmail.com>
2026-05-08 14:14:46 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -9,24 +10,46 @@ import Login from "./pages/Login";
 import Result from "./pages/Result";
 import History from "./pages/History";
 
-const routerBaseName =
-  import.meta.env.BASE_URL === "/" ? undefined : import.meta.env.BASE_URL;
-
 export default function App() {
+  const [darkMode, setDarkMode] = useState(() => {
+    const savedTheme = localStorage.getItem("theme");
+    return savedTheme === "dark";
+  });
+
+  useEffect(() => {
+    localStorage.setItem("theme", darkMode ? "dark" : "light");
+
+    if (darkMode) {
+      document.body.classList.add("dark-body");
+    } else {
+      document.body.classList.remove("dark-body");
+    }
+  }, [darkMode]);
+
   return (
-    <BrowserRouter basename={routerBaseName}>
-      <Navbar />
+    <div className={darkMode ? "dark" : "light"}>
+      <BrowserRouter>
+        <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/generator" element={<Generator />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/result" element={<Result />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
+        <Routes>
+          <Route path="/" element={<Home darkMode={darkMode} />} />
 
-      <Footer />
-    </BrowserRouter>
+          <Route
+            path="/generator"
+            element={<Generator darkMode={darkMode} />}
+          />
+
+          <Route path="/login" element={<Login darkMode={darkMode} />} />
+
+          <Route path="/result" element={<Result darkMode={darkMode} />} />
+
+          <Route path="/history" element={<History darkMode={darkMode} />} />
+
+          <Route path="/about" element={<About darkMode={darkMode} />} />
+        </Routes>
+
+        <Footer />
+      </BrowserRouter>
+    </div>
   );
 }
